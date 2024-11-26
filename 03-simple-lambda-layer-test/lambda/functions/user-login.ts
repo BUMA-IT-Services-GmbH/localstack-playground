43eabb308c61684aad53d87967b9b3bd7cdebcbe
Mario Buber <mario.buber@buma-its.com>
2024-11-26 20:23:08 +0100

    update localstack projects

--- 03-simple-lambda-layer-test/lambda/functions/user-login.ts
+++ 03-simple-lambda-layer-test/lambda/functions/user-login.ts
@@ -8,19 +8,21 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 let combinedOutput = '';
 
   try {
-    console.log('========== function content ==========');
+    const functionContent = '========== function content ==========\n';
     const varTaskLa = execSync('ls -la /var/task').toString();
     const varTaskR = execSync('ls -R /var/task').toString();
+    combinedOutput += functionContent;
     combinedOutput += 'Contents of /var/task (ls -la):\n' + varTaskLa + '\n';
     combinedOutput += 'Contents of /var/task (ls -R):\n' + varTaskR + '\n';
 
-    console.log('========== layer content ==========');
-    const optLa = execSync('ls -la /opt').toString();
-    const optR = execSync('ls -R /opt').toString();
-    combinedOutput += 'Contents of /opt (ls -la):\n' + optLa + '\n';
-    combinedOutput += 'Contents of /opt (ls -R):\n' + optR + '\n';
+    const layerContent = '========== layer content ==========\n';
+    const optNodejsLa = execSync('ls -la /opt/nodejs').toString();
+    const opNodejstR = execSync('ls -R /opt/nodejs').toString();
+    combinedOutput += layerContent;
+    combinedOutput += 'Contents of /opt/nodejs (ls -la):\n' + optNodejsLa + '\n';
+    combinedOutput += 'Contents of /opt/nodejs (ls -R):\n' + opNodejstR + '\n';
 
-    console.log(combinedOutput)
+    console.log('\n' + combinedOutput);
   } catch (error) {
     console.error('Error listing directories:', error);
   }
